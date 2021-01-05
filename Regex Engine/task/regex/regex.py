b6fde825bbaeb9312c2e9609e701053577e9ddc8
cptironmand <jaymandjunk@gmail.com>
2021-01-04 19:54:17 -0600

    2.0 Regex engine works

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,5 +1,5 @@
 
-def input_string():
+def input_strings():
     the_input = input()
     index_no = the_input.index("|")
     reg = the_input[0:index_no]
@@ -10,34 +10,36 @@ def input_string():
 def quick_compare(reg, ts):
     if reg in ts:
         return True
+    elif reg == ts:
+        return True
     elif not reg:
         return True
     elif not ts:
         return False
     elif (reg == "") and (ts == ""):
         return True
+    else:
+        return False
 
 
 def evaluate(reg, ts):
     ev = quick_compare(reg, ts)
+    #print(f"ev = {ev}")
     if ev:
         return True
-    else:
-        str_len = len(ts)
-        reg_len = len(reg)
-
-        if reg_len > str_len:
+    if reg and ts:
+        if (reg[0] == ".") or (reg[0] == ts[0]):
+            reg_ev = reg[1:]
+            #print(reg_ev)
+            ts_ev = ts[1:]
+            #print(ts_ev)
+            return evaluate(reg_ev, ts_ev)
+        elif (reg[0] != ts[0]):
             return False
-
-        if "." in reg:
-            if reg_len <= str_len:
-                dot_index = reg.index(".")
-                reg_minus_dot = reg[dot_index+1:]
-                if reg_minus_dot in ts:
-                    return True
-    return False
+    else:
+        return False
 
 
 # Main Body
-regex, the_string = input_string()
+regex, the_string = input_strings()
 print(evaluate(regex, the_string))
