d79662f98f9dd7fa282d0696b988a52aa847c2fb
cptironmand <jaymandjunk@gmail.com>
2021-01-25 20:55:12 -0600

    6.0 Complete

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -2,10 +2,8 @@
 # ts is the string variable in a function
 # _ev means the variable has been evaluated
 
-import sys
-sys.setrecursionlimit(10000)
-
 counter = 0
+
 
 def input_strings():
     the_input = input()
@@ -15,12 +13,42 @@ def input_strings():
     return reg, ts
 
 
+def checkdowns(reg):
+    slash_in = False
+    start_in = False
+    end_in = False
+    question_in = False
+    star_in = False
+    plus_in = False
+    if "\\" in reg:
+        slash_in = True
+    if "^" in reg:
+        if "^" == reg[0]:
+            start_in = True
+    if "$" in reg:
+        if "$" == reg[-1] and "\\" != reg[-2]:
+            end_in = True
+    if "?" in reg:
+        q_index = reg.index("?")
+        if reg[q_index-1] != "\\":
+            question_in = True
+    if "*" in reg:
+        s_index = reg.index("*")
+        if reg[s_index-1] != "\\":
+            star_in = True
+    if "+" in reg:
+        p_index = reg.index("+")
+        if reg[p_index-1] != "\\":
+            plus_in = True
+    return slash_in, start_in, end_in, question_in, star_in, plus_in
+
+
 def quick_compare(reg, ts):
     global counter
     if counter == 0:
         if reg in ts:
             return True
-        counter +=1
+        counter += 1
     if reg == ts:
         return True
     elif not reg:
@@ -31,6 +59,25 @@ def quick_compare(reg, ts):
         return True
     else:
         return False
+
+
+def remove_slash(reg):
+    reg_ev = ""
+    s = 0  # Count of all slashes
+    t = 0  # running total slashes
+    for letter in reg:
+        if letter == "\\":
+            s += 1
+
+    for letter in reg:
+        if letter != "\\":
+            reg_ev += letter
+        if letter == "\\":
+            t += 1
+            i = reg.index(letter)
+            if reg[i+1] == "\\" and s > t:
+                reg_ev += letter
+    return reg_ev
 
 
 def starting(reg, ts):
@@ -47,61 +94,53 @@ def ending(reg, ts):
 
 
 def check_questionmark(reg, ts):  # Preceding char occurs 0 or 1 times
-    if "?" in reg:
-        ind = reg.index("?")
-        # Preceding char occurs 1 times
-        if (len(reg) - (len(ts))) == 1:   #  Check for only the '$'
-            reg_ev = reg[0:ind] + reg[ind+1:]
-            return reg_ev
-        # Preceding char occurs 0 times
-        elif (len(reg) - (len(ts)) == 2):  # Check for the "$' plus 1 char
-            reg_ev = reg[0:ind-1] + reg[ind+1:]
-            return reg_ev
-        else:
-            reg_ev = reg[0:ind] + reg[ind+1:]
+    ind = reg.index("?")
+    # Preceding char occurs 1 times
+    if (len(reg) - (len(ts))) == 1:   # Check for only the '$'
+        reg_ev = reg[0:ind] + reg[ind+1:]
+        return reg_ev
+    # Preceding char occurs 0 times
+    elif len(reg) - (len(ts)) == 2:  # Check for the "$' plus 1 char
+        reg_ev = reg[0:ind-1] + reg[ind+1:]
         return reg_ev
     else:
-        return reg
+        reg_ev = reg[0:ind] + reg[ind+1:]
+    return reg_ev
 
 
 def check_star(reg, ts):  # Preceding char occurs 0 or more times
-    if "*" in reg:
-        ind = reg.index("*")
-        # Preceding char occurs 0 times
-        if (len(reg) - (len(ts)) == 2):
-            reg_ev = reg[0:ind - 1] + reg[ind + 1:]
-        # Preceding character occurs 1 or more times
-        else:
-            regtest = reg[0:ind] + reg[ind+1:]
-            need = len(ts) - len(regtest)
-            reg_ev = reg[0:ind] + (reg[ind-1]*need) + reg[ind+1:]
-        return reg_ev
+    ind = reg.index("*")
+    # Preceding char occurs 0 times
+    if len(reg) - len(ts) == 2:
+        reg_ev = reg[0:ind - 1] + reg[ind + 1:]
+    # Preceding character occurs 1 or more times
     else:
-        return reg
+        reg_test = reg[0:ind] + reg[ind+1:]
+        need = len(ts) - len(reg_test)
+        reg_ev = reg[0:ind] + (reg[ind-1]*need) + reg[ind+1:]
+    return reg_ev
 
 
 def check_plus(reg, ts):  # Preceding char occurs 1 or more times
-    if "+" in reg:
-        ind = reg.index("+")
-        char = reg[ind-1]
-        count = 0
-        beg = reg[0:ind]
-        end = reg[ind+1:]
+    ind = reg.index("+")
+    char = reg[ind-1]
+    count = 0
+    beg = reg[0:ind]
+    end = reg[ind+1:]
 
-        # loop to find max number of reg chars
-        for l in ts:
-            if l == char:
-                count +=1
+    # loop to find max number of reg chars
+    for l in ts:
+        if l == char:
+            count +=1
 
-        # Build a work in progress regex and test it while you build it for the length of 'count'
-        for i in range(count):
-            ev = evaluate((beg + char*i + end), ts)
-            if ev:
-                return (beg + char*i + end)
+    # Build a work in progress regex and test it while you build it for the length of 'count'
+    for i in range(count):
+        ev = evaluate((beg + char*i + end), ts)
+        if ev:
+            return (beg + char*i + end)
 
-        return (beg + end)
-    else:
-        return reg
+    return (beg + end)
+
 
 def check_start_end(reg, ts):
     # Check starting AND ending
@@ -128,8 +167,6 @@ def check_start_end(reg, ts):
             else:
                 reg = orig_reg
                 return reg, ts
-        #elif len(reg) >= len(ts):
-         #   return reg, ts
         else:
             reg = orig_reg
             return reg, ts
@@ -159,15 +196,31 @@ def evaluate(reg, ts):
     else:
         return False
 
+
 # Main Body
 regex, the_string = input_strings()
-#print(f'the regex after the input string function is {regex} and the string is {the_string}')
-regex, the_string = check_start_end(regex, the_string)
-#print(f'the regex after checking for starting / ending chars is {regex} and the string is {the_string}')
-regex = check_questionmark(regex, the_string)
-#print(f'the regex after we check question mark is {regex} and the string is {the_string}')
-regex = check_star(regex, the_string)
-#print(f'the regex after we check star is {regex} and the string is {the_string}')
-regex = check_plus(regex, the_string)
-#print(f'the regex after we check plus is {regex} and the string is {the_string}')
+
+slash, start, end, question, star, plus = checkdowns(regex)
+
+if slash:
+    regex = remove_slash(regex)
+
+if start and end:
+    regex, the_string = check_start_end(regex, the_string)
+
+if start and not end:
+    regex, the_string = starting(regex, the_string)
+
+if end and not start:
+    regex, the_string = ending(regex, the_string)
+
+if question:
+    regex = check_questionmark(regex, the_string)
+
+if star:
+    regex = check_star(regex, the_string)
+
+if plus:
+    regex = check_plus(regex, the_string)
+
 print(evaluate(regex, the_string))
